146889caec4e0541a33651bbe0f25d3f5ec872d4
nosus <carol_hsu@trend.com.tw>
2016-05-10 19:45:19 +0800

    add strict condition

--- kubernetes/recipes/kubernetes-minion-setup.rb
+++ kubernetes/recipes/kubernetes-minion-setup.rb
@@ -2,15 +2,32 @@ include_recipe 'kubernetes::kubernetes'
 
 bash "minion-file-copy" do
     user 'root'
-    cwd '/tmp'
+    cwd '/tmp/kubernetes/server/kubernetes/server/bin'
     code <<-EOH
-    if ! [[ $(ls /usr/local/bin/kube*) ]]; then
-      mkdir /var/lib/kubelet
-      mkdir /etc/kubernetes
-	  cd ./kubernetes/server/kubernetes/server/bin
-      cp kubelet kube-proxy /usr/local/bin/
-    fi
+    if [[ $(ls /usr/local/bin/kubelet) ]]; then
+		current_version=$(/usr/local/bin/kubelet --version | awk -F"Kubernetes v" '{ print $2 }')
+		if [ "$current_version" -eq "#{node['kubernetes']['version']}" ]; then
+        	exit
+    	fi
+	fi
+    cp kubelet kube-proxy /usr/local/bin/
     EOH
+end
+
+directory '/var/lib/kubelet' do
+	owner 'root'
+	group 'root'
+	mode '0755'
+	subscribes :create, "bash[minion-file-copy]", :immediately
+    action :nothing
+end
+
+directory '/etc/kubernetes' do
+	owner 'root'
+	group 'root'
+	mode '0755'
+	subscribes :create, "bash[minion-file-copy]", :immediately
+    action :nothing
 end
 
 template "/etc/init.d/kubernetes-minion" do
--- kubernetes/recipes/kubernetes.rb
+++ kubernetes/recipes/kubernetes.rb
@@ -2,11 +2,24 @@ bash 'install_kubernetes' do
   user 'root'
   cwd '/tmp'
   code <<-EOH
-  if ! [[ $(ls /usr/local/bin/kube*) ]]; then
-    wget --max-redirect 255 https://github.com/GoogleCloudPlatform/kubernetes/releases/download/v#{node['kubernetes']['version']}/kubernetes.tar.gz
-    tar zxvf kubernetes.tar.gz
-    cd kubernetes/server
-    tar zxvf kubernetes-server-linux-amd64.tar.gz
+  if [[ $(ls /usr/local/bin/kubectl) ]]; then
+	current_version=$(/usr/local/bin/kubectl version | awk 'NR==1' | awk -F":\"v" '{ print $2 }' | awk -F"\"," '{ print $1 }')
+	if [ "$current_version" -eq "#{node['kubernetes']['version']}" ]; then
+		exit
+	fi
   fi
+
+  if [[ $(ls /usr/local/bin/kubelet) ]] ; then
+	current_version=$(/usr/local/bin/kubelet --version | awk -F"Kubernetes v" '{ print $2 }')
+    if [ "$current_version" -eq "#{node['kubernetes']['version']}" ]; then
+        exit
+    fi
+  fi
+  rm -rf kubernetes/
+  wget --max-redirect 255 https://github.com/GoogleCloudPlatform/kubernetes/releases/download/v#{node['kubernetes']['version']}/kubernetes.tar.gz -O kubernetes-#{node['kubernetes']['version']}.tar.gz
+  tar zxvf kubernetes-#{node['kubernetes']['version']}.tar.gz
+  cd kubernetes/server
+  tar zxvf kubernetes-server-linux-amd64.tar.gz
+
   EOH
 end
